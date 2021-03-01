ce62139c9141b1292e5b52aeaaad1bff21665391
RainbowCrow <RainbowCrow@users.noreply.github.com>
2021-03-02 00:15:16 +0700

    update

--- js/detail-rp.js
+++ js/detail-rp.js
@@ -6,3 +6,45 @@ Array.from(document.querySelectorAll('[data-expand]'), (input) => {
     input.addEventListener('input', updateSize);
     updateSize();
 });
+
+$('.special #make-a-grade').click(function (e) {
+    e.preventDefault();
+    if ($('.grade').css('display') === 'block') {
+        $('.grade').hide(500);
+    } 
+    else $('.grade').show(500);
+});
+mouse_is_inside_grade = false;
+$('.grade').hover(function () {
+    mouse_is_inside_grade = true;
+}, function () {
+    mouse_is_inside_grade = false;
+});
+
+$('#make-a-grade').hover(function() {
+    mouse_is_inside_btn_grade = true;
+}, function () {
+    mouse_is_inside_btn_grade = false;
+})
+
+$("body").mouseup(function () {
+    if (!mouse_is_inside_grade && !mouse_is_inside_btn_grade && ($('#make-a-grade').css('display') == 'block')) $(".grade").hide(500);
+});
+
+$('#disable').click(function(e) {
+    e.preventDefault();
+    $('#make-a-grade').hide(500);
+    $(this).hide(500);
+    $('#active').show(500);
+    $('.grade').css('transform', 'none');
+    $('#status').text('Disable');
+});
+
+$('#active').click(function(e) {
+    e.preventDefault();
+    $('#make-a-grade').hide(500);
+    $(this).hide(500);
+    $('#disable').show(500);
+    $('.grade').css('transform', 'none');
+    $('#status').text('Active');
+});
